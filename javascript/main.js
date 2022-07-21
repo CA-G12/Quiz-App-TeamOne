e47ae385224cf0fd3a7bc8c361b661f6d4c2245e
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-07-21 13:24:04 +0300

    javascript/main.js is Finished Relase #10

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+const start = document.getElementById("start");
+const userName = document.getElementById("userName");
+var user = { username: "", score: 0 };
+
+start.addEventListener("click", () => {
+  user.username = userName.value;
+  user.score = 0;
+
+  localStorage.setItem("user", JSON.stringify(user));
+  console.log(user);
+  window.location.href = "Html/quiz.html";
+});
