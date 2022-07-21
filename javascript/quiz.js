090dc0b2162e283d6b32c1b18c05c19f9cb8df7c
moustf <tepro.18@gmail.com>
2022-07-21 13:48:17 +0300

    Holded the needed elements for the dom with variables relates #13

---
+++ javascript/quiz.js
@@ -0,0 +1,13 @@
+// ? Adding the needed html elements.
+const questionNum = document.getElementsByClassName("ques-num")[0];
+const questionText = document.querySelector(".question");
+const firstChoice = document.querySelector(".first-choice"),
+  isFirstSelected = firstChoice.querySelector(".class");
+const secondChoice = document.querySelector(".second-choice"),
+  isSecondSelected = firstChoice.querySelector(".class");
+const thirdChoice = document.querySelector(".third-choice"),
+  isThirdSelected = firstChoice.querySelector(".class");
+const forthChoice = document.querySelector(".forth-choice"),
+  isForthSelected = firstChoice.querySelector(".class");
+
+
